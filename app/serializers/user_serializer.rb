ce9de3177242b13cd435916253d8ff6b8f8be317
gozmeister <samgorick.sg@gmail.com>
2020-07-06 10:14:08 -0500

    current month txns

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -6,4 +6,11 @@ class UserSerializer < ActiveModel::Serializer
   has_many :savings
   has_many :saving_values, through: :savings
 
+  # Returns current month transactions only
+  def transactions 
+    time = Time.now
+    test_date = "#{time.strftime('%Y')}-#{time.strftime('%m')}-01 00:00:00 +0000"
+    @object.transactions.select { |txn| txn.created_at > test_date }
+  end
+
 end
