3968b5a19dec1f172a90cbf55ce0fafd2f1c14a1
gozmeister <samgorick.sg@gmail.com>
2020-07-20 16:53:31 -0500

    saving params

--- app/controllers/savings_controller.rb
+++ app/controllers/savings_controller.rb
@@ -2,11 +2,11 @@ class SavingsController < ApplicationController
 
   def create
     # Find saving category
-    category = SavingCategory.find_by(name: params[:category])
+    category = SavingCategory.find_by(name: saving_params[:category])
     # Create saving instance
-    saving = Saving.create!(name: params[:name], user_id: params[:user_id], saving_category_id: category.id)
+    saving = Saving.create!(name: saving_params[:name], user_id: saving_params[:user_id], saving_category_id: category.id)
     # Create savings value
-    value = SavingValue.create!(saving_id: saving.id, value: params[:balance])
+    value = SavingValue.create!(saving_id: saving.id, value: saving_params[:balance])
     # Render json of savings with saving_values attached to each
     render json: saving
   end
@@ -14,6 +14,12 @@ class SavingsController < ApplicationController
   def index
     savings = Saving.all
     render json: savings
+  end
+
+  private
+
+  def saving_params
+    params.require(:saving).permit(:name, :category, :balance, :user_id)
   end
 
 end
