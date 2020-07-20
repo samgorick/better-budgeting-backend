9cbc339b9a136833eea16744c39f901633b97a34
gozmeister <samgorick.sg@gmail.com>
2020-07-20 17:14:55 -0500

    saving values params

--- app/controllers/saving_values_controller.rb
+++ app/controllers/saving_values_controller.rb
@@ -1,14 +1,20 @@
 class SavingValuesController < ApplicationController
 
   def create
-    value = SavingValue.create(saving_id: params[:id], value: params[:amount])
-    saving = Saving.find(params[:id])
+    value = SavingValue.create(saving_params)
+    saving = Saving.find(saving_params[:saving_id])
     render json: saving
   end
 
   def index
     savings = SavingValue.all
     render json: savings
+  end
+
+  private
+
+  def saving_params
+    params.require(:saving).permit(:saving_id, :value)
   end
 
 end
